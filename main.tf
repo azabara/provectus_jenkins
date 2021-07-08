4190d2454b6680eae169db3beb71115e37e45988
azabara <46056342+azabara@users.noreply.github.com>
2021-07-09 00:22:38 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "jenkins-instance" {
 resource "aws_security_group" "sg_allow_ssh_jenkins" {
   name        = "allow_ssh_jenkins"
   description = "Allow SSH and Jenkins inbound traffic"
-  vpc_name    = "provectus" 
+  vpc_id      = "provectus" 
 
   ingress {
     from_port   = 22
