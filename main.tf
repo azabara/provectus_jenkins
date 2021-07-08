5ad4b589fcdc27b307a0f76ad8d4479a1feaa1a4
azabara <46056342+azabara@users.noreply.github.com>
2021-07-09 00:50:38 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "jenkins-instance" {
 resource "aws_security_group" "sg_allow_ssh_jenkins" {
   name        = "allow_ssh_jenkins"
   description = "Allow SSH and Jenkins inbound traffic"
-  vpc_id      = vpc.id
+  vpc_id      = "vpc-06d4ab0a55916ebff"
 
   ingress {
     from_port   = 22
